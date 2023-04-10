ec3664ba76ddbc19c63faa9b1ecd371536fdc254
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-04-10 04:57:52 -0400

    implement scrolltotop component

--- flask_app/client/src/main.jsx
+++ flask_app/client/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './component/ScrollToTop'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
+            <ScrollToTop />
             <App />
         </BrowserRouter>
   </React.StrictMode>,
