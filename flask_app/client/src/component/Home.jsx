b906e177b471a83f382d0979800232916c9760dc
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-04-10 14:51:12 -0400

    massive visuals overhaul in progress

--- flask_app/client/src/component/Home.jsx
+++ flask_app/client/src/component/Home.jsx
@@ -5,22 +5,32 @@ import "./Home.css"
 const Home = () => {
   return (
       <>
-        <div className="flex flex-col gap-10 items-center bg-">
-            <h1 className='text-[5rem] md:text-[7rem] font-normal mt-[4rem] uppercase text-center text-mines'>Latest Episode</h1>
-              <div className="flex flex-col justify-between gap-4 bg-mines-950 rounded py-6 px-8 mb-5 min-h-[120px] w-full md:w-1/4">
-                  <div className='h-full w-full bg-mines-800 rounded px-5'>This is a div</div>
-            </div>
+        <div className="flex flex-col items-center gap-10">
+            <h1 className='text-[2.2rem] font-medium md:font-normal md:text-[7rem] mt-[4rem] uppercase text-center text-[2D2D2D]'>
+                Welcome to Tolkien With Friends
+            </h1>
+                <h2 className="text-[1.3rem] md:text-4xl font-medium">
+                    Let's Explore Together
+              </h2>
+              <div className="w-full md:w-1/4 flex flex-col gap-5 items-center text-center w-1/3">
+
+                    <hr className='border-t-1 border-mines w-3/4 mt-3' />
+                    <p className="text-mines font-medium text-[1.3rem] md:text-[2rem]">Join Andie, Kali and friends as they re-discover Middle-earth together by comparing the works of Tolkien to their various adaptations, starting with Peter Jackson's take on <i>Lord of the Rings</i> and <i>The Hobbit</i>.</p>
+                    <hr className="border-t-1 border-mines w-3/4 mb-2" />
+                    <Link to="about" className="flex items-center justify-center hover:bg-mines-500 hover:bg-opacity-10 text-mines border border-mines rounded-lg px-3 py-1 mb-[8rem]"><p>MORE ABOUT US</p></Link>
+                </div> 
             {/* <img className="self-start" src="./img/hill 2.svg" alt="" /> */}
           </div>
-
-        <div className="flex flex-col gap-10 items-center py-10 bg-[url('../img/mountains.jpg')]">
-            <div className="w-full md:w-1/4 flex flex-col gap-5 items-center text-center w-1/3">
-                <hr className='border-t-1 border-white w-3/4 mt-3' /> 
-                <p className="text-white text-[1.3rem] md:text-[2rem]">Join Andie, Kali and friends as they re-discover Middle-earth together by comparing the works of Tolkien to their various adaptations, starting with Peter Jackson's take on <i>Lord of the Rings</i> and <i>The Hobbit</i>.</p>
-                <hr className="border-t-1 border-white w-3/4 mb-2" />
-                <Link to="about" className="flex items-center justify-center hover:bg-white hover:bg-opacity-10 text-white border border-white rounded-lg px-3 py-1"><p>MORE ABOUT US</p></Link>
-            </div> 
-        </div>
+          <div className="">
+                <div className="flex flex-col gap-10 items-center py-10 bg-lime-950">
+                    <h1 className='text-[7rem] font-normal mt-[4rem] uppercase text-center text-white'>
+                        LATEST EPISODE
+                  </h1>
+                  <div>
+                      
+                  </div>
+                </div>
+          </div>
 
         <div className="diag"></div>
 
